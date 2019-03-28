c4d224d7746e625b656c4970ddc96118036c67f4
Gustavo Salibi <gustavo.salibi@gmail.com>
2019-03-28 00:02:22 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
-get_latest_release() {
-  curl --silent "https://api.github.com/repos/hashicorp/terraform/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                                             # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                                     # Pluck JSON value
-}
-
 github_version=$(cat github_version.txt)
-del_version=$(cat delete_version.txt)
 ftp_version=$(cat ftp_version.txt)
+del_version=$(cat delete_version.txt)
 
 if [ $github_version != $ftp_version ]
 then
@@ -19,6 +13,7 @@ then
     XC_OS=linux XC_ARCH=ppc64le make bin
     cd bin
     mv terraform terraform-$github_version
+    if [[ $github_version > $ftp_version ]]
     then
           lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/terraform/latest terraform-$github_version" 
           lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/terraform/terraform-$ftp_version" 
