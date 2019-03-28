47cea02d6c863085243e953c336015a15c15babd
Gustavo Salibi <gustavo.salibi@gmail.com>
2019-03-28 00:26:55 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,16 @@ then
     unzip v$github_version.zip
     mv terraform-$github_version terraform
     cd terraform
+    
+    # This script is used by the Travis build to install a cookie for
+    # go.googlesource.com so rate limits are higher when using `go get` to fetch
+    # packages that live there.
+    # See: https://github.com/golang/go/issues/12933
+    bash scripts/gogetcookie.sh
+    make tools
+    
+    git config --global url.https://github.com/.insteadOf ssh://git@github.com/  
+    
     XC_OS=linux XC_ARCH=ppc64le make bin
     cd bin
     mv terraform terraform-$github_version
