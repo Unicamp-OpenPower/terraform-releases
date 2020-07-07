68969ff61868902039c3a52dbb8ac03366575387
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-07-06 23:00:38 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-github_version=$(cat github_version.txt)
-ftp_version=$(cat ftp_version.txt)
-del_version=$(cat delete_version.txt)
+#github_version=$(cat github_version.txt)
+#ftp_version=$(cat ftp_version.txt)
+ftp_version=0.12.25
+github_version=0.12.26
+#del_version=$(cat delete_version.txt)
 
 if [ "$github_version" != "$ftp_version" ] 
 then
@@ -16,11 +18,11 @@ then
     sudo chmod 777 terraform
     ./terraform --version
     mv terraform terraform-$github_version
-    if [[ "$github_version" > "$ftp_version" ]]
-    then
-          lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/terraform/latest terraform-$github_version" 
-          lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/terraform/latest/terraform-$ftp_version" 
-    fi
+    #if [[ "$github_version" > "$ftp_version" ]]
+    #then
+    #      lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/terraform/latest terraform-$github_version" 
+    #      lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/terraform/latest/terraform-$ftp_version" 
+    #fi
     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/terraform terraform-$github_version" 
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/terraform/terraform-$del_version" 
+    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/terraform/terraform-$del_version" 
 fi
